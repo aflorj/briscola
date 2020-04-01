9536b9988015dac4ea9dfd6daffd2a70b5b831e4
aflorj <florjancicaljaz@gmail.com>
2020-04-01 22:07:04 +0200

    Little things

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
     <h1>Briscola</h1>
-    <h2>h2</h2>
     </>
   );
 }
--- src/cards.js
+++ src/cards.js
@@ -1,332 +1,345 @@
-// Every card as a sepparate object
+// Every card as a separate object
 // https://en.wikipedia.org/wiki/Briscola
 // Italian 40-card deck: 10x of: denari (coins), spade (swords), coppe (cups), bastoni (batons)
 // Face cards: fante (knave), cavallo (knight), re (king)
 
-// coins
+// COINS
 
 const twoOfCoins = {
     suit: 'coins',
     strength: 1,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Two of Coins'
-};
-
-const fourOfCoins = {
+  };
+  
+  const fourOfCoins = {
     suit: 'coins',
     strength: 2,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Four of Coins'
-};
-
-const fiveOfCoins = {
+  };
+  
+  const fiveOfCoins = {
     suit: 'coins',
     strength: 3,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Five of Coins'
-};
-
-const sixOfCoins = {
+  };
+  
+  const sixOfCoins = {
     suit: 'coins',
     strength: 4,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Six of Coins'
-};
-
-const sevenOfCoins = {
+  };
+  
+  const sevenOfCoins = {
     suit: 'coins',
     strength: 5,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Seven of Coins'
-};
-
-const knaveOfCoins = {
+  };
+  
+  const knaveOfCoins = {
     suit: 'coins',
     strength: 6,
     points: 2,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knave of Coins'
-};
-
-const knightOfCoins = {
+  };
+  
+  const knightOfCoins = {
     suit: 'coins',
     strength: 7,
     points: 3,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knight of Coins'
-};
-
-const kingOfCoins = {
+  };
+  
+  const kingOfCoins = {
     suit: 'coins',
     strength: 8,
     points: 4,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'King of Coins'
-};
-
-const threeOfCoins = {
+  };
+  
+  const threeOfCoins = {
     suit: 'coins',
     strength: 9,
     points: 10,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Three of Coins'
-};
-
-const aceOfCoins = {
+  };
+  
+  const aceOfCoins = {
     suit: 'coins',
     strength: 10,
     points: 11,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Ace of Coins'
-};
-
-// swords
-
-const twoOfSwords = {
+  };
+  
+  // SWORDS
+  
+  const twoOfSwords = {
     suit: 'swords',
     strength: 1,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Two of Swords'
-};
-
-const fourOfSwords = {
+  };
+  
+  const fourOfSwords = {
     suit: 'swords',
     strength: 2,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Four of Swords'
-};
-
-const fiveOfSwords = {
+  };
+  
+  const fiveOfSwords = {
     suit: 'swords',
     strength: 3,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Five of Swords'
-};
-
-const sixOfSwords = {
+  };
+  
+  const sixOfSwords = {
     suit: 'swords',
     strength: 4,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Six of Swords'
-};
-
-const sevenOfSwords = {
+  };
+  
+  const sevenOfSwords = {
     suit: 'swords',
     strength: 5,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Seven of Swords'
-};
-
-const KnaveOfSwords = {
+  };
+  
+  const knaveOfSwords = {
     suit: 'swords',
     strength: 6,
     points: 2,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knave of Swords'
-};
-
-const knightOfSwords = {
+  };
+  
+  const knightOfSwords = {
     suit: 'swords',
     strength: 7,
     points: 3,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knight of Swords'
-};
-
-const kingOfSwords = {
+  };
+  
+  const kingOfSwords = {
     suit: 'swords',
     strength: 8,
     points: 4,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'King of Swords'
-};
-
-const threeOfSwords = {
+  };
+  
+  const threeOfSwords = {
     suit: 'swords',
     strength: 9,
     points: 10,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Three of Swords'
-};
-
-const aceOfSwords = {
+  };
+  
+  const aceOfSwords = {
     suit: 'swords',
     strength: 10,
     points: 11,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Ace of Swords'
-};
-
-// cups
-
-const twoOfCups = {
+  };
+  
+  // CUPS
+  
+  const twoOfCups = {
     suit: 'cups',
     strength: 1,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Two of Cups'
-};
-
-const fourOfCups = {
+  };
+  
+  const fourOfCups = {
     suit: 'cups',
     strength: 2,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Four of Cups'
-};
-
-const fiveOfCups = {
+  };
+  
+  const fiveOfCups = {
     suit: 'cups',
     strength: 3,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Five of Cups'
-};
-
-const sixOfCups = {
+  };
+  
+  const sixOfCups = {
     suit: 'cups',
     strength: 4,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Six of Cups'
-};
-
-const sevenOfCups = {
+  };
+  
+  const sevenOfCups = {
     suit: 'cups',
     strength: 5,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Seven of Cups'
-};
-
-const KnaveOfCups = {
+  };
+  
+  const knaveOfCups = {
     suit: 'cups',
     strength: 6,
     points: 2,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knave of Cups'
-};
-
-const knightOfCups = {
+  };
+  
+  const knightOfCups = {
     suit: 'cups',
     strength: 7,
     points: 3,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knight of Cups'
-};
-
-const kingOfCups = {
+  };
+  
+  const kingOfCups = {
     suit: 'swords',
     strength: 8,
     points: 4,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'King of Cups'
-};
-
-const threeOfCups = {
+  };
+  
+  const threeOfCups = {
     suit: 'cups',
     strength: 9,
     points: 10,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Three of Cups'
-};
-
-const aceOfCups = {
+  };
+  
+  const aceOfCups = {
     suit: 'cups',
     strength: 10,
     points: 11,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Ace of Cups'
-};
-
-// batons
-
-const twoOfBatons = {
+  };
+  
+  // BATONS
+  
+  const twoOfBatons = {
     suit: 'batons',
     strength: 1,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Two of Batons'
-};
-
-const fourOfBatons = {
+  };
+  
+  const fourOfBatons = {
     suit: 'batons',
     strength: 2,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Four of Batons'
-};
-
-const fiveOfBatons = {
+  };
+  
+  const fiveOfBatons = {
     suit: 'batons',
     strength: 3,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Five of Batons'
-};
-
-const sixOfBatons = {
+  };
+  
+  const sixOfBatons = {
     suit: 'batons',
     strength: 4,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Six of Batons'
-};
-
-const sevenOfBatons = {
+  };
+  
+  const sevenOfBatons = {
     suit: 'batons',
     strength: 5,
     points: 0,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Seven of Batons'
-};
-
-const KnaveOfBatons = {
+  };
+  
+  const knaveOfBatons = {
     suit: 'batons',
     strength: 6,
     points: 2,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knave of Batons'
-};
-
-const knightOfBatons = {
+  };
+  
+  const knightOfBatons = {
     suit: 'swords',
     strength: 7,
     points: 3,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Knight of Batons'
-};
-
-const kingOfBatons = {
+  };
+  
+  const kingOfBatons = {
     suit: 'swords',
     strength: 8,
     points: 4,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'King of Batons'
-};
-
-const threeOfBatons = {
+  };
+  
+  const threeOfBatons = {
     suit: 'swords',
     strength: 9,
     points: 10,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Three of Batons'
-};
-
-const aceOfBatons = {
+  };
+  
+  const aceOfBatons = {
     suit: 'batons',
     strength: 10,
     points: 11,
-    image: 'image-path',
+    imagePath: 'image-url',
     alt: 'Ace of Batons'
-};
+  };
+  
+  // A full deck array
+  
+  const deck = [
+    twoOfCoins, fourOfCoins, fiveOfCoins, sixOfCoins, sevenOfCoins, knaveOfCoins, knightOfCoins, kingOfCoins, threeOfCoins, aceOfCoins,
+    twoOfSwords, fourOfSwords, fiveOfSwords, sixOfSwords, sevenOfSwords, knaveOfSwords, knightOfSwords, kingOfSwords, threeOfSwords, aceOfSwords,
+    twoOfCups, fourOfCups, fiveOfCups, sixOfCups, sevenOfCups, knaveOfCups, knightOfCups, kingOfCups, threeOfCups, aceOfCups,
+    twoOfBatons, fourOfBatons, fiveOfBatons, sixOfBatons, sevenOfBatons, knaveOfBatons, knightOfBatons, kingOfBatons, threeOfBatons, aceOfBatons
+  ];
+
+  function deckBuilder(arrayOfCards) {
+      return arrayOfCards.forEach(e => e.suit);
+  }
