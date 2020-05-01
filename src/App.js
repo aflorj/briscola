eaa693de6a8bed7bb3499cd815084078686eaf5e
aflorj <florjancicaljaz@gmail.com>
2020-05-01 21:34:33 +0200

    added some setup values

--- src/App.js
+++ src/App.js
@@ -21,4 +21,4 @@ const App = Client({ game: Briscola });
 export default App;
 
 // shuffledDeck.splice(shuffledDeck.length - 3, 3)
-// Use this to deal first round of the cards to each player.
+// Use this to deal first round of the cards to each player.headToHead: [],headToHead: [],
