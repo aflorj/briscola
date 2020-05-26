a742f9f102039b168c9bb50d270d3a7ac2c7860f
aflorj <florjancicaljaz@gmail.com>
2020-05-26 11:08:48 +0200

    game logic

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,75 +1,66 @@
-import { shuffledDeck } from './cards.js';
-import { TurnOrder } from 'boardgame.io/core';
+import { shuffledDeck } from "./cards.js";
+import { TurnOrder } from "boardgame.io/core";
 
 export const Briscola = {
-  name: 'Briscola',
+  name: "Briscola",
 
   setup: prepareGame,
 
   phases: {
-
     draw: {
-      moves: { },
+      moves: {},
       onBegin: (G, ctx) => {
-
         G.player_0.cards = G.deckOnBoard.splice(G.deckOnBoard.length - 3, 3);
         G.player_1.cards = G.deckOnBoard.splice(G.deckOnBoard.length - 3, 3);
         G.briscola = G.deckOnBoard.pop();
-
       },
 
-      next: 'play',
+      next: "play",
       start: true,
-      endIf: G => G.briscola !== null
-
+      endIf: (G) => G.briscola !== null,
     },
 
     play: {
       moves: { playCard },
-      next: 'compare',
-      endIf: G => G.player_0.played !== null && G.player_1.played !== null
-
+      next: "compare",
+      endIf: (G) => G.player_0.played !== null && G.player_1.played !== null,
     },
 
     compare: {
-      moves: { },
+      moves: {},
       onBegin: evaluate,
-      next: 'play',
-      onEnd: cleanup
-    }
+      onEnd: cleanup,
+      next: "play",
+    },
   },
-  turn: {
-    
-  }
+  turn: {},
 };
 
 function prepareGame() {
   return {
-       player_0: {
-        cards: [],
-        picked: [],
-        played: null
-      },
-        
-      player_1: {
-        cards: [],
-        picked: [],
-        played: null
-      }, 
+    player_0: {
+      cards: [],
+      picked: [],
+      played: null,
+    },
+
+    player_1: {
+      cards: [],
+      picked: [],
+      played: null,
+    },
 
     briscola: null,
     deckOnBoard: shuffledDeck,
-
   };
-};
-
+}
 
 function playCard(G, ctx, cardID) {
-  let playerID = 'player_' + ctx.currentPlayer;
+  let playerID = "player_" + ctx.currentPlayer;
   let currentPlayer = G[playerID];
   currentPlayer.played = currentPlayer.cards.splice(cardID, 1)[0];
   ctx.events.endTurn();
-};
+}
 
 function evaluate(G, ctx) {
   let briscola = G.briscola;
@@ -78,47 +69,41 @@ function evaluate(G, ctx) {
   let p0picked = G.player_0.picked;
   let p1picked = G.player_1.picked;
 
-  // TODO
-  // endPhase ne dela, treba je nrdit se turn order.
-  
   if (p0.suit === p1.suit) {
     if (p0.strength > p1.strength) {
-      console.log('suit enak, po moci pobere p0');
+      console.log("suit enak, po moci pobere p0");
       p0picked.push(p0);
       p0picked.push(p1);
       ctx.events.endPhase();
-
-        } else {
-      console.log('suit enak, po moci pobere p1');
+    } else {
+      console.log("suit enak, po moci pobere p1");
       p1picked.push(p0);
       p1picked.push(p1);
       ctx.events.endPhase();
     }
   } else if (briscola.suit === p0.suit && briscola.suit !== p1.suit) {
-    console.log('p0 ima briscolo in pobere');
+    console.log("p0 ima briscolo in pobere");
     p0picked.push(p0);
     p0picked.push(p1);
     ctx.events.endPhase();
-
   } else if (briscola.suit !== p0.suit && briscola.suit === p1.suit) {
-    console.log('p1 ima briscolo in pobere');
+    console.log("p1 ima briscolo in pobere");
     p1picked.push(p0);
     p1picked.push(p1);
     ctx.events.endPhase();
-
   } else {
-    console.log('Pobere WOLR')
-    let winnerOfLastRound = 'player_' + ctx.playOrder[0];
+    console.log("Pobere WOLR");
+    let winnerOfLastRound = "player_" + ctx.playOrder[0];
     G[winnerOfLastRound].picked.push(p0);
     G[winnerOfLastRound].picked.push(p1);
     ctx.events.endPhase();
   }
+}
 
-};
-
-function cleanup(G){
+function cleanup(G) {
   G.player_0.played = null;
   G.player_1.played = null;
+  console.log("I should be cleaning up right now...");
 }
 
 /* ctx.events.endTurn({ next: playerID }); */
