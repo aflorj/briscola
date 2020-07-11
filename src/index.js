05b603eb2f248ef6c5a944409640b5fa73524854
aflorj <florjancicaljaz@gmail.com>
2020-07-11 23:13:35 +0200

    Heroku

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./styles/popup.css";
+import App from "./App.js";
+import * as serviceWorker from "./serviceWorker.js";
+import Popup from "react-popup";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App playerID="0" />
+    </BrowserRouter>
+    <Popup></Popup>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
